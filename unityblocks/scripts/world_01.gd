c780e2406829a033f904c7dbae78123dfd076c45
yasmartns <yasmingomes2977@gmail.com>
2025-02-27 22:28:05 -0300

    Atualizando

---
+++ unityblocks/scripts/world_01.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func become_host():
+	print("Become host pressed")
+	%MultiplayerHUD.hide()
+	MultiplayerManager.become_host()
+	
+func join_as_player_2():
+	print("Join as player 2")
+	%MultiplayerHUD.hide()
+	MultiplayerManager.join_as_player_2()
