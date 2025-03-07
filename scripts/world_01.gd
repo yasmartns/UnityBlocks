79c0a4b0efac79486b114bba33d78ebe276e3b3e
yasmartns <160919616+yasmartns@users.noreply.github.com>
2025-03-07 14:07:23 -0300

    Revert "teste"
    This reverts commit c31b0e02cd780d5e2144a3199b6f40dc9fbb76b8.

--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -9,8 +9,3 @@ func join_as_player_2():
 	print("Join as player 2")
 	%MultiplayerHUD.hide()
 	MultiplayerManager.join_as_player_2()
-	
-@onready var camera := $camera as Camera2D
-
-func _ready() -> void:
-	pass
