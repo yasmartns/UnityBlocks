ea4ff4cd87927c84099078e1031cd8f4294e46d0
Yasmin Martins <160919616+yasmartns@users.noreply.github.com>
2025-04-03 01:07:58 -0300

    final

--- scripts/inicio_local.gd
+++ scripts/inicio_local.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://levels/tutorial.tscn")
+	get_tree().change_scene_to_file("res://levels/world_01.tscn")
 
 
 func _on_settings_pressed() -> void:
